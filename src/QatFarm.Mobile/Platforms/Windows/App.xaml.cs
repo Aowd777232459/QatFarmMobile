f5ab5bdc943d06257acc565983ea7d8a7eecd735
Aowd777232459 <bdalmlkmhsnhsynw@gmail.com>
2026-09-03 00:27:47 +0300

    platform: wire Windows MAUI host

---
+++ src/QatFarm.Mobile/Platforms/Windows/App.xaml.cs
@@ -0,0 +1,15 @@
+#if WINDOWS
+using Microsoft.UI.Xaml;
+
+namespace QatFarm.Mobile.WinUI;
+
+public partial class App : MauiWinUIApplication
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+#endif
