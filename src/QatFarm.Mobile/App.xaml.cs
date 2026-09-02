03071e56e3cf4dc70286d9884beab86f05a3da23
Aowd777232459 <bdalmlkmhsnhsynw@gmail.com>
2026-09-03 00:35:53 +0300

    windows: open ERP in desktop-sized workspace

--- src/QatFarm.Mobile/App.xaml.cs
+++ src/QatFarm.Mobile/App.xaml.cs
@@ -4,6 +4,15 @@ public partial class App : Application
 {
     public App() => InitializeComponent();
 
-    protected override Window CreateWindow(IActivationState? activationState) =>
-        new(new MainPage()) { Title = "نظام زراعي عواد سوفت" };
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = new Window(new MainPage()) { Title = "نظام زراعي عواد سوفت" };
+#if WINDOWS
+        window.Width = 1450;
+        window.Height = 900;
+        window.MinimumWidth = 980;
+        window.MinimumHeight = 650;
+#endif
+        return window;
+    }
 }
