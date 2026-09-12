a3e4b797fea99e9b2c52f2ff2a3bc8ddd070a2f5
Aowd777232459 <bdalmlkmhsnhsynw@gmail.com>
2026-09-12 16:03:16 +0300

    windows: add WinUI startup class

---
+++ src/QatFarm.Mobile/Platforms/Windows/App.xaml.cs
@@ -0,0 +1,13 @@
+using Microsoft.UI.Xaml;
+
+namespace QatFarm.Mobile.WinUI;
+
+public partial class App : MauiWinUIApplication
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
